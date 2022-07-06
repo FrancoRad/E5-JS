f7e194e8997e4266b849d99afb625954b18f2385
FrancoRad <franco.rad15@gmail.com>
2022-07-05 21:01:34 -0300

    TP E5 JS Nucba

---
+++ pizzas.js
@@ -0,0 +1,56 @@
+export const pizzas = [
+    {
+      id: 1,
+      nombre: "Mozzarella",
+      ingredientes: ["harina", "agua", "tomate", "mozzarella"],
+      precio: "$"+400,
+      imagen: "https://thumbs.dreamstime.com/b/pizza-cuatro-quesos-en-un-tablero-de-madera-sabrosa-218042441.jpg"
+    },
+    {
+      id: 2,
+      nombre: "4 quesos",
+      ingredientes: [
+        "harina",
+        "agua",
+        "mozzarella",
+        "gruyere",
+        "roquefort"
+      ],
+      precio: "$"+500,
+      imagen: "https://www.bofrost.es/writable/products/images-v2/01774.jpg"
+    },
+    {
+      id: 3,
+      nombre: "Especial",
+      ingredientes: ["harina", "agua", "tomate", "queso", "jamon", "huevo"],
+      precio: "$"+650,
+      imagen: "https://media-cdn.tripadvisor.com/media/photo-s/0e/0a/27/05/pizza-especial-salsa.jpg"
+    },
+    {
+      id: 4,
+      nombre: "Rucula",
+      ingredientes: [
+        "harina",
+        "agua",
+        "queso",
+        "crudo",
+        "rucula"
+      ],
+      precio: "$"+800,
+      imagen: "https://static-sevilla.abc.es/media/gurmesevilla/2015/03/pizza-de-tomatitos-cherry-con-rucula.jpg"
+    },
+    {
+      id: 5,
+      nombre: "Cantimpalo",
+      ingredientes: ["harina", "agua", "tomate", "mozzarella", "cantimpalo"],
+      precio: "$"+650,
+      imagen: "https://www.lavanguardia.com/files/image_948_465/uploads/2017/11/21/5e9977a58c570.jpeg"
+    },
+    {
+      id: 6,
+      nombre: "Napolitana",
+      ingredientes: ["harina", "agua", "tomate", "mozzarella", "oregano"],
+      precio: "$"+550,
+      imagen: "https://okdiario.com/img/2016/08/15/pizza-napolitana-sin-gluten.jpg"
+    }
+  ]
